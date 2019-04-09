b765dc7909303874b05d13a1eb7a06d032c75b3c
Antoine Harel <antoine.harel@epita.fr>
2019-04-09 23:34:19 +0200

    [SERVER] Added static serving in prod mode

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 const config = require('config')
 const dotenv = require('dotenv');
+const path = require('path');
 
 // ============== Setup  ==============
 dotenv.config();
@@ -24,6 +25,13 @@ mongoose.connect(dbConfig.mongoURI,
 // ======== Routes Declaration =========
 app.use('/api/auth', require('./routes/api/auth'));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // =========== Server Launch ===========
 app.listen(port, () => console.log(`[EXPRESS] Server is running on http://localhost:${port}`.cyan));
